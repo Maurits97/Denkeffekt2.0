e4d1b160da526ce17d7c829055d0ed0685bcaffb
Maurits Brouwer <>
2020-11-05 20:36:09 +0100

    new styles

--- js/index.js
+++ js/index.js
@@ -13,3 +13,21 @@ function myMap() {
 	var map = new google.maps.Map(document.getElementById("map"), mapOptions);
 	var marker = new google.maps.Marker({position: Overveen, map: map});
 }
+
+//navigation animation
+const hamburger = document.querySelector('#nav__hamburger');
+const overlay = document.querySelector('.overlay');
+const overlayMenu = document.querySelector('.overlay__menu');
+const exit = document.querySelector('.overlay__exit');
+
+hamburger.addEventListener('click', function(){
+    overlay.style.clipPath = 'circle(100%)';
+    overlayMenu.style.opacity = 1;
+    overlayMenu.style.marginTop = '100px';
+})
+
+exit.addEventListener('click', function(){
+    overlay.style.clipPath = 'circle(0%)';
+    overlayMenu.style.opacity = 0;
+    overlayMenu.style.marginTop = '80px';
+})
